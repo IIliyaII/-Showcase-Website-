371f50182467cc39ff7f8e9baf8f87a124bdd7c9
IIliyaII) <eeleyash@gmail.com>
2026-07-15 22:57:33 +0330

    create info , logo and github link

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import streamlit as st
+import pandas
 
-column1, column2 = st.columns(2)
+
+
+st.set_page_config(layout="wide")
+
+column1, column2= st.columns(2)
 
 with column1:
     st.image('images/GitHub-logo.png')
 
 with column2:
-    st.title(f"Hello, World!\n"
-                f"I am Iliya, a Computer Engineering student, an aspiring back-end developer, and a cybersecurity enthusiast.\n"
+    st.title(f"Hello , I am Iliya! \n")
+    st.info(f" Computer Engineering student, an aspiring back-end developer, and a cybersecurity enthusiast.\n"
                 f"Here, you can explore some of my projects and their GitHub repositories.")
+
+df=pandas.read_csv("images\data.csv",sep=";")
+column3 ,empty_column, column4 = st.columns([1.5,0.5,1.5])
+with column3:
+
+        for index, row in df[:10].iterrows():
+            st.header(row['title'])
+            st.image(f"images/{row['image']}")
+            st.info(row['description'])
+            st.write(f"[Source code]({row['url']})")
+with column4:
+
+
+        for index, row in df[10:].iterrows():
+            st.header(row['title'])
+            st.image(f"images/{row['image']}")
+            st.info(row['description'])
+            st.write(f"[Source code]({row['url']})")
+
